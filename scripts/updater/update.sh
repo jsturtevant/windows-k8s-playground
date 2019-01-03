7a3b2033ae5e02b6b4fb0bb365295d8134addea8
James Sturtevant <jstur@microsoft.com>
2019-01-03 12:12:28 -0800

    no longer needs pub ip

--- scripts/updater/update.sh
+++ scripts/updater/update.sh
@@ -4,25 +4,26 @@ set -x
 
 # usage: 
 #   export WIN_PASS=<yourpass>
-#   ./update.sh 
+#   ./update.sh <masterip>
 #
 # currently assumes binaries are in ~
+PUBIP=${1}
+KEY_FILE=${2:-"~/.ssh/id_rsa"}
+SSH_USER=${3:-"adminuser"}
+NEW_BINARIES=${4:-"k8s-binaries"}
+PACKAGES=${5:-"k8s-packages"}
 
-KEY_FILE=${1:-"~/.ssh/id_rsa"}
-SSH_USER=${2:-"adminuser"}
-NEW_BINARIES=${3:-"k8s-binaries"}
-PACKAGES=${4:-"k8s-packages"}
 
 updateNode(){
-    echo "add script and files"
-    scp -i "$KEY_FILE" win-updater.ps1 "$SSH_USER@$2:"
+    echo "add files"
+    scp -i "$KEY_FILE" -o "ProxyCommand ssh -i $KEY_FILE -W %h:%p $SSH_USER@$PUBIP" win-updater.ps1 "$SSH_USER@$2:"
     pushd ~/
-    scp -i "$KEY_FILE" "$PACKAGES/kubebinaries_$dt.tar.gz" "$SSH_USER@$2:"
+    scp -i "$KEY_FILE" -o "ProxyCommand ssh -i $KEY_FILE -W %h:%p $SSH_USER@$PUBIP" "$PACKAGES/kubebinaries_$dt.tar.gz" "$SSH_USER"@"$2": 
     popd
 
     echo "kick off update"
     kubectl drain $1
-    ssh -t -i "$KEY_FILE" "$SSH_USER@$2" "powershell -f win-updater.ps1 $3"
+    ssh -t -i "$KEY_FILE" -o "ProxyCommand ssh -i $KEY_FILE -W %h:%p $SSH_USER@$PUBIP" "$SSH_USER@$2" "powershell -f win-updater.ps1 $3"
     kubectl uncordon $1
 }
 
@@ -33,11 +34,11 @@ tar -cvzhf "$PACKAGES/kubebinaries_$dt.tar.gz" $NEW_BINARIES
 popd
 
 echo "updating vm"
-windowsnodes="$(kubectl get nodes -o json | jq -rc '.items | map(select(.metadata.labels."beta.kubernetes.io/os" | contains("windows"))?) | map({ nodename: .metadata.name, pubip: .status.addresses | map(select(.type | contains("ExternalIP"))?) | .[] | .address  })[]')"
+windowsnodes="$(kubectl get nodes -o json | jq -rc '.items | map(select(.metadata.labels."beta.kubernetes.io/os" | contains("windows"))?) | map({ nodename: .metadata.name, nodeip: .status.addresses | map(select(.type | contains("InternalIP"))?) | .[] | .address  })[]')"
 while read -r node; do
     echo "$node"
     nodename="$(echo "$node" | jq -r .nodename)"
-    ip="$(echo "$node" | jq -r .pubip)"
+    ip="$(echo "$node" | jq -r .nodeip)"
     echo "updating vm $nodename with ip $ip..."
     updateNode $nodename $ip kubebinaries_$dt.tar.gz
 done <<< "$windowsnodes"
