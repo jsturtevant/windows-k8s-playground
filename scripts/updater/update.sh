7754708593d659a3c31a56f85b50783ceefdc658
James Sturtevant <jstur@microsoft.com>
2019-01-02 20:19:09 -0800

    updates all windows nodes with public ips

--- scripts/updater/update.sh
+++ scripts/updater/update.sh
@@ -1,29 +1,45 @@
 #!/usr/bin/env bash
 
+set -x
+
 # usage: 
 #   export WIN_PASS=<yourpass>
-#   ./update.sh 12.34.45.6 3472k8s000 
+#   ./update.sh 
+#
+# currently assumes binaries are in ~
 
-IP=$1
-NODE=$2
-KEY_FILE=${3:-"~/.ssh/id_rsa"}
-SSH_USER=${4:-"adminuser"}
-NEW_BINARIES=${5:-"k8s-binaries"}
-PACKAGES=${6:-"k8s-packages"}
+KEY_FILE=${1:-"~/.ssh/id_rsa"}
+SSH_USER=${2:-"adminuser"}
+NEW_BINARIES=${3:-"k8s-binaries"}
+PACKAGES=${4:-"k8s-packages"}
+
+updateNode(){
+    echo "add script and files"
+    scp -i "$KEY_FILE" win-updater.ps1 "$SSH_USER@$2:"
+    pushd ~/
+    scp -i "$KEY_FILE" "$PACKAGES/kubebinaries_$dt.tar.gz" "$SSH_USER@$2:"
+    popd
+
+    echo "kick off update"
+    kubectl drain $1
+    ssh -t -i "$KEY_FILE" "$SSH_USER@$2" "powershell -f win-updater.ps1 $3"
+    kubectl uncordon $1
+}
 
 echo "zip files"
 dt="$(date '+%m%d%Y-%H%M%S')"
 pushd ~/
-tar -cvzf "$PACKAGES/kubebinaries_$dt.tar.gz" $NEW_BINARIES
+tar -cvzhf "$PACKAGES/kubebinaries_$dt.tar.gz" $NEW_BINARIES
 popd
 
-echo "add script and files"
-scp -i "$KEY_FILE" win-updater.ps1 "$SSH_USER@$IP:"
-scp -i "$KEY_FILE" "~/$PACKAGES/kubebinaries_$dt.tar.gz" "$SSH_USER@$IP:"
-
-echo "kick off update"
-kubectl drain $NODE
-ssh -t -i "$KEY_FILE" "$SSH_USER@$IP" "powershell -f win-updater.ps1 kubebinaries_$dt.tar.gz"
-kubectl uncordon $NODE
+echo "updating vm"
+windowsnodes="$(kubectl get nodes -o json | jq -rc '.items | map(select(.metadata.labels."beta.kubernetes.io/os" | contains("windows"))?) | map({ nodename: .metadata.name, pubip: .status.addresses | map(select(.type | contains("ExternalIP"))?) | .[] | .address  })[]')"
+while read -r node; do
+    echo "$node"
+    nodename="$(echo "$node" | jq -r .nodename)"
+    ip="$(echo "$node" | jq -r .pubip)"
+    echo "updating vm $nodename with ip $ip..."
+    updateNode $nodename $ip kubebinaries_$dt.tar.gz
+done <<< "$windowsnodes"
 
 echo "complete windows update"
