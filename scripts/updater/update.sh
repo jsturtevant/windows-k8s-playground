79337402e30d28c89569c62d0be41567610ab8fb
James Sturtevant <jstur@microsoft.com>
2019-01-10 14:20:14 -0800

    no longer needs binaries in specific dir

--- scripts/updater/update.sh
+++ scripts/updater/update.sh
@@ -6,31 +6,34 @@ set -x
 #   export WIN_PASS=<yourpass>
 #   ./update.sh <masterip>
 #
-# currently assumes binaries are in ~
+# 
 PUBIP=${1}
 KEY_FILE=${2:-"~/.ssh/id_rsa"}
 SSH_USER=${3:-"adminuser"}
-NEW_BINARIES=${4:-"k8s-binaries"}
-PACKAGES=${5:-"k8s-packages"}
+NEW_BINARIES=${4:-"$HOME/k8s-binaries"}
+PACKAGES=${5:-"$HOME/k8s-packages"}
 
 
 updateNode(){
+    echo "make folders if don't exist"
+    ssh -t -i "$KEY_FILE" -o "ProxyCommand ssh -i $KEY_FILE -W %h:%p $SSH_USER@$PUBIP" "$SSH_USER@$2" "powershell -c mkdir c:/temp/dev"
+    
     echo "add files"
-    scp -i "$KEY_FILE" -o "ProxyCommand ssh -i $KEY_FILE -W %h:%p $SSH_USER@$PUBIP" win-updater.ps1 "$SSH_USER@$2:"
-    pushd ~/
-    scp -i "$KEY_FILE" -o "ProxyCommand ssh -i $KEY_FILE -W %h:%p $SSH_USER@$PUBIP" "$PACKAGES/kubebinaries_$dt.tar.gz" "$SSH_USER"@"$2": 
+    scp -i "$KEY_FILE" -o "ProxyCommand ssh -i $KEY_FILE -W %h:%p $SSH_USER@$PUBIP" win-updater.ps1 "$SSH_USER@$2:c:/temp/dev/"
+    pushd "$PACKAGES"
+    scp -i "$KEY_FILE" -o "ProxyCommand ssh -i $KEY_FILE -W %h:%p $SSH_USER@$PUBIP" "kubebinaries_$dt.tar.gz" "$SSH_USER"@"$2:c:/temp/dev/" 
     popd
 
     echo "kick off update"
     kubectl drain $1
-    ssh -t -i "$KEY_FILE" -o "ProxyCommand ssh -i $KEY_FILE -W %h:%p $SSH_USER@$PUBIP" "$SSH_USER@$2" "powershell -f win-updater.ps1 $3"
+    ssh -t -i "$KEY_FILE" -o "ProxyCommand ssh -i $KEY_FILE -W %h:%p $SSH_USER@$PUBIP" "$SSH_USER@$2" "powershell -f c:/temp/dev/win-updater.ps1 $3"
     kubectl uncordon $1
 }
 
 echo "zip files"
 dt="$(date '+%m%d%Y-%H%M%S')"
-pushd ~/
-tar -cvzhf "$PACKAGES/kubebinaries_$dt.tar.gz" $NEW_BINARIES
+pushd "$NEW_BINARIES"
+tar -cvzhf "$PACKAGES/kubebinaries_$dt.tar.gz" .
 popd
 
 echo "updating vm"
--- scripts/updater/win-updater.ps1
+++ scripts/updater/win-updater.ps1
@@ -4,7 +4,7 @@ Param(
 )
 
 # Constants
-$packageDestination = "$HOME"
+$packageDestination = "c:\temp\dev"
 
 Stop-Service kubelet -Force
 Stop-Service containerd -Force
@@ -19,15 +19,15 @@ if(!(Test-path "c:\temp\extracted\$package")) {
 tar -xzf "$packageDestination\$package" -C c:\temp\extracted\$package
 
 Write-Host "update azure cni" 
-cp "c:\temp\extracted\$package\k8s-binaries\azurecni\azure-vnet-ipam.exe" "c:\k\azurecni\bin\" -Force
-cp "c:\temp\extracted\$package\k8s-binaries\azurecni\azure-vnet.exe" "c:\k\azurecni\bin\" -Force
-cp "c:\temp\extracted\$package\k8s-binaries\azurecni\10-azure.conflist" "c:\k\azurecni\conflist" -Force
+cp "c:\temp\extracted\$package\azurecni\azure-vnet-ipam.exe" "c:\k\azurecni\bin\" -Force
+cp "c:\temp\extracted\$package\azurecni\azure-vnet.exe" "c:\k\azurecni\bin\" -Force
+cp "c:\temp\extracted\$package\azurecni\10-azure.conflist" "c:\k\azurecni\conflist" -Force
 
 Write-Host "update containerd" 
-cp "c:\temp\extracted\$package\k8s-binaries\containerd\*" "c:\containerd" -Force -Recurse
+cp "c:\temp\extracted\$package\containerd\*" "c:\containerd" -Force -Recurse
 
 Write-Host "update kube binaries and files" 
-cp "c:\temp\extracted\$package\k8s-binaries\kube\*" "C:\k\" -Force -Recurse
+cp "c:\temp\extracted\$package\kube\*" "C:\k\" -Force -Recurse
 
 start-service containerd
 Start-Service kubelet
