f05daf083c422296b4aa926cdb38941bf6f52ade
James Sturtevant <jsturtevant@gmail.com>
2019-02-04 16:15:48 -0800

    use scp :rocket:

--- scripts/kubebuild/copy-out.sh
+++ scripts/kubebuild/copy-out.sh
@@ -5,8 +5,9 @@
 #   ./copy-out.sh 12.34.45.6   
 
 MASTERIP=$1
-BINARY_LOCATION=${2:-"~/k8s-binaries/"}
-SSH_USER=${3:-"adminuser"}
+BINARY_LOCATION=${2:-"$HOME/k8s-binaries/kube"}
+SSH_USER=${3:-"jstur"}
 KEY_FILE=${4:-"~/.ssh/id_rsa"}
 
-ssh -t -i "$KEY_FILE" "$SSH_USER@$MASTERIP":~/go/src/k8s.io/kubernetes/_output/dockerized/bin/windows/amd64/* $BINARY_LOCATION
+scp -i "$KEY_FILE" "$SSH_USER@$MASTERIP":~/go/src/k8s.io/kubernetes/_output/dockerized/bin/windows/amd64/kubelet.exe $BINARY_LOCATION
+scp -i "$KEY_FILE" "$SSH_USER@$MASTERIP":~/go/src/k8s.io/kubernetes/_output/dockerized/bin/windows/amd64/kube-proxy.exe $BINARY_LOCATION
