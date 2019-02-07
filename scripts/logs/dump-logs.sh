40c43437d4fc80ae7b7d43f054131dcd78c9ed4d
James Sturtevant <jsturtevant@gmail.com>
2019-02-07 09:25:47 -0800

    update log puller

--- scripts/logs/dump-logs.sh
+++ scripts/logs/dump-logs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 # usage: 
 #   export WIN_PASS=<yourpass>
 #   ./dump-logs.ssh 12.34.45.6   
@@ -8,7 +10,21 @@ MASTERIP=$1
 SSH_USER=${2:-"adminuser"}
 KEY_FILE=${3:-"~/.ssh/id_rsa"}
 
-scp -i "$KEY_FILE" collect-logs.sh  "$SSH_USER@$MASTERIP:"
-ssh -t -i "$KEY_FILE" "$SSH_USER@$MASTERIP" "./collect-logs.sh $SSH_USER $WIN_PASS $MASTERIP"
-mkdir -p $MASTERIP
-scp -i "$KEY_FILE" -r "$SSH_USER@$MASTERIP:$MASTERIP/" $MASTERIP
+
+collectlogs(){
+    mkdir -p $HOME/k8s-logs/$MASTERIP/$2
+    scp -o "ProxyCommand ssh -W %h:%p $SSH_USER@$MASTERIP" $SSH_USER@$2:'c:\k\*.log' $HOME/k8s-logs/$MASTERIP/$1
+}
+
+echo "store logs by master ip"
+mkdir -p $HOME/k8s-logs/$MASTERIP
+
+echo "colleting windows logs"
+windowsnodes="$(kubectl get nodes -o json | jq -rc '.items | map(select(.metadata.labels."beta.kubernetes.io/os" | contains("windows"))?) | map({ nodename: .metadata.name, nodeip: .status.addresses | map(select(.type | contains("InternalIP"))?) | .[] | .address  })[]')"
+while read -r node; do
+    echo "$node"
+    nodename="$(echo "$node" | jq -r .nodename)"
+    ip="$(echo "$node" | jq -r .nodeip)"
+    echo "updating vm $nodename with ip $ip..."
+    collectlogs $nodename $ip
+done <<< "$windowsnodes"
